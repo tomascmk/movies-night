12b1ba39b18b71343f6a2469bfcd456c4b17c6ef
tomascmk <tomascormack@gmail.com>
2024-03-03 11:08:31 -0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ module.exports = {
         "@typescript-eslint/strict-boolean-expressions": "off",
         "@typescript-eslint/indent": "off",
         "@typescript-eslint/no-extraneous-class": "off",
-        "@typescript-eslint/space-before-function-paren": "off"
+        "@typescript-eslint/space-before-function-paren": "off",
+
     }
 }
