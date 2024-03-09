b078736c1b8ff17445d5aa545cb3522a82477e76
tomascmk <tomascormack@gmail.com>
2024-03-09 13:16:04 -0300

    404 page modified

--- src/components/Error/ErrorDashboard.tsx
+++ src/components/Error/ErrorDashboard.tsx
@@ -5,28 +5,20 @@ export const ErrorDashboard = () => {
   return (
     <Container className='mt-4'>
       <Row>
-        <Alert
-          key=''
-          variant='primary'
-          className='mt-5 d-flex justify-content-center'
-        >
-          We couldn't find the page you are looking for. You can
-          <Alert.Link href='/' style={{ marginLeft: 4 }}>
-            return to dashboard
-          </Alert.Link>
-          .
+        <Alert key='' variant='info' className='mt-5'>
+          <Alert.Heading>Page not found.</Alert.Heading>
+          We couldn't find the page you are looking for.
+          <hr />
+          <div className='d-flex justify-content-center'>
+            <Button href='/' variant='info'>
+              Return
+            </Button>
+          </div>
         </Alert>
       </Row>
       <Row>
         <Col className='d-flex justify-content-center mt-5'>
           <Image src='../../../public/img/closed-cinema.png' />
-        </Col>
-      </Row>
-      <Row>
-        <Col className='d-flex justify-content-center mt-5'>
-          <Button href='/' variant='primary'>
-            Home
-          </Button>
         </Col>
       </Row>
     </Container>
