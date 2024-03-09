3454a4c75860e40e7c10d2e0e6a8004c4526ac32
tomascmk <tomascormack@gmail.com>
2024-03-09 15:58:07 -0300

    Navbar conditions added

--- src/components/Navbar/NavbarMenu.tsx
+++ src/components/Navbar/NavbarMenu.tsx
@@ -1,6 +1,8 @@
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap'
 
 export const NavbarMenu = () => {
+  const canShowUserOptions = false
+
   return (
     <Navbar className='bg-body-tertiary'>
       <Container>
@@ -14,17 +16,20 @@ export const NavbarMenu = () => {
           />{' '}
           Movies Night
         </Navbar.Brand>
-        <Nav>
-          <Nav.Link href='/signup'>Sign up</Nav.Link>
-          <Nav.Link href='/signin'>Sign in</Nav.Link>
-        </Nav>
-        <NavDropdown title='User' id='navbarScrollingDropdown'>
-          <NavDropdown.Item href='#action3'>Profile</NavDropdown.Item>
-          <NavDropdown.Item href='#action4'>Wishlist</NavDropdown.Item>
-          <NavDropdown.Item href='#action4'>Favourite</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href='#action5'>Sign out</NavDropdown.Item>
-        </NavDropdown>
+        {canShowUserOptions ? (
+          <NavDropdown title='User' id='navbarScrollingDropdown'>
+            <NavDropdown.Item href='#action3'>Profile</NavDropdown.Item>
+            <NavDropdown.Item href='#action4'>Wishlist</NavDropdown.Item>
+            <NavDropdown.Item href='#action4'>Favourite</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href='#action5'>Sign out</NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <Nav>
+            <Nav.Link href='/signup'>Sign up</Nav.Link>
+            <Nav.Link href='/signin'>Sign in</Nav.Link>
+          </Nav>
+        )}
       </Container>
     </Navbar>
   )
